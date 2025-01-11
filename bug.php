ce122072487e1a0ff96e59d3697d9e7bce4cc24b
BugHunter-X <askzenai@gmail.com>
2025-01-11 12:46:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,22 @@
+function foo($a, $b) {
+  if ($a === $b) {
+    return true;
+  }
+  if (is_array($a) && is_array($b)) {
+    if (count($a) !== count($b)) {
+      return false;
+    }
+    foreach ($a as $key => $value) {
+      if (!array_key_exists($key, $b) || $value !== $b[$key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+//This function will not correctly compare arrays with different key orders
+$array1 = ['a' => 1, 'b' => 2];
+$array2 = ['b' => 2, 'a' => 1];
+var_dump(foo($array1, $array2)); //false, should be true
